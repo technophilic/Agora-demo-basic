dd3fd11d496b9e7f26a543b20ec22be5cef9a009
technophilic <svineth.face@gmail.com>
2021-03-09 01:27:33 +0530

    updated the demo for NG SDK

--- scripts/script.js
+++ scripts/script.js
@@ -1,85 +1,87 @@
-/**
- * @name handleFail
- * @param err - error thrown by any function
- * @description Helper function to handle errors
- */
-let handleFail = function(err){
-    console.log("Error : ", err);
-};
-
-// Queries the container in which the remote feeds belong
+// JS reference to the container where the remote feeds belong
 let remoteContainer= document.getElementById("remote-container");
 
 /**
  * @name addVideoStream
- * @param streamId
+ * @param uid - uid of the user
  * @description Helper function to add the video stream to "remote-container"
  */
-function addVideoStream(streamId){
+function addVideoStream(uid){
     let streamDiv=document.createElement("div"); // Create a new div for every stream
-    streamDiv.id=streamId;                       // Assigning id to div
+    streamDiv.id=uid;                       // Assigning id to div
     streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
     remoteContainer.appendChild(streamDiv);      // Add new div to container
 }
 /**
  * @name removeVideoStream
- * @param evt - Remove event
+ * @param uid - uid of the user
  * @description Helper function to remove the video stream from "remote-container"
  */
-function removeVideoStream (evt) {
-    let stream = evt.stream;
-    stream.stop();
-    let remDiv=document.getElementById(stream.getId());
-    remDiv.parentNode.removeChild(remDiv);
-
-    console.log("Remote stream is removed " + stream.getId());
+function removeVideoStream (uid) {
+    let remDiv=document.getElementById(uid);
+    remDiv && remDiv.parentNode.removeChild(remDiv);
 }
 
 
 
-document.getElementById("start").onclick = function () {
+document.getElementById("start").onclick = async function () {
+    // Client Setup
+    // Defines a client for RTC
+    const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
-// Client Setup
-// Defines a client for RTC
-    let client = AgoraRTC.createClient({
-        mode: 'live',
-        codec: "h264"
+    // Get credentials from the form
+    let appId = document.getElementById("app-id").value;
+    let channelId = document.getElementById("channel").value;
+    let token = document.getElementById("token").value || null;
+
+    // Create local tracks
+    const [localAudioTrack, localVideoTrack] = await AgoraRTC.createMicrophoneAndCameraTracks();
+    
+    // Initialize the stop button
+    initStop(client, localAudioTrack, localVideoTrack);
+    
+    // Play the local track
+    localVideoTrack.play('me');
+
+    // Set up event listeners for remote users publishing or unpublishing tracks
+    client.on("user-published", async (user, mediaType) => {
+        await client.subscribe(user, mediaType); // subscribe when a user publishes
+        if (mediaType === "video") {
+          addVideoStream(String(user.uid)) // uses helper method to add a container for the videoTrack
+          user.videoTrack.play(String(user.uid));
+        }
+        if (mediaType === "audio") {
+          user.audioTrack.play();
+        }
+    });
+    client.on("user-unpublished",  async (user, mediaType) => {
+        if (mediaType === "video") {
+            removeVideoStream(user.uid)
+        }
     });
 
-// Client Setup
-let appid = document.getElementById("app-id").value;
-let channelid="any-channel";
-let userid;
-
-
-// Defines a client for Real Time Communication
-    client.init(appid,() => console.log("AgoraRTC client initialized") ,handleFail);
-
-// The client joins the channel
-    client.join(null,channelid,String(Date.now()).substr(7), (uid)=>{
-        
-        var localStream = AgoraRTC.createStream({
-            video: true,
-            audio: false,
-        });
-        localStream.init(function(){
-            localStream.play('me');
-            client.publish(localStream); // Publish it to the channel
-        });
-        console.log(`App id : ${appid}\nChannel id : ${channelid}\nUser id : ${uid}`);
-
-    },handleFail);
-//When a stream is added to a channel
-    client.on('stream-added', function (evt) {
-        client.subscribe(evt.stream, handleFail);
-    });
-//When you subscribe to a stream
-    client.on('stream-subscribed', function (evt) {
-        let stream = evt.stream;
-        addVideoStream(stream.getId());
-        stream.play(stream.getId());
-    });
-//When a person is removed from the stream
-    client.on('stream-removed',removeVideoStream);
-    client.on('peer-leave',removeVideoStream);
+    // Join a channnel and retrieve the uid for local user
+    const _uid = await client.join(appId, channelId, token, null); 
+    await client.publish([localAudioTrack, localVideoTrack]);
 };
+
+function initStop(client, localAudioTrack, localVideoTrack){
+    const stopBtn = document.getElementById('stop');
+    stopBtn.disabled = false; // Enable the stop button
+    stopBtn.onclick = null; // Remove any previous event listener
+    stopBtn.onclick = function () {
+        client.unpublish(); // stops sending audio & video to agora
+        localVideoTrack.stop(); // stops video track and removes the player from DOM
+        localVideoTrack.close(); // Releases the resource
+        localAudioTrack.stop();  // stops audio track
+        localAudioTrack.close(); // Releases the resource
+        client.remoteUsers.forEach(user => {
+            if (user.hasVideo) {
+                removeVideoStream(user.uid) // Clean up DOM
+            }
+            client.unsubscribe(user); // unsubscribe from the user
+        });
+        client.removeAllListeners(); // Clean up the client object to avoid memory leaks
+        stopBtn.disabled = true;
+    }
+}
